2e5e0a33ff3a7f3d26980499216c970152ac1f17
Abhay Mehta <114793909+Abhaymehta1424@users.noreply.github.com>
2025-08-17 20:14:05 +0530

    Time: 51 ms (67.96%) | Memory: 62.5 MB (43.69%) - LeetSync

---
+++ 2834-relocate-marbles/relocate-marbles.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> relocateMarbles(int[] nums, int[] moveFrom, int[] moveTo) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            set.add(num);
+        }
+        for(int i =0;i<moveFrom.length;i++){
+            set.remove(moveFrom[i]);
+            set.add(moveTo[i]);
+        }
+
+        List<Integer> res = new ArrayList<>(set);
+        Collections.sort(res);
+
+        return res;
+    }
+}
