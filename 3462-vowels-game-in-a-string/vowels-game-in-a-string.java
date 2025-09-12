be1a8bd56b2749f4f77ee9704a66ad5e5571ad2f
Abhay Mehta <114793909+Abhaymehta1424@users.noreply.github.com>
2025-09-12 10:32:10 +0530

    Time: 37 ms (5.25%) | Memory: 45.6 MB (66.93%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        Set<Character> vowelset = Set.of('a','e','i','o','u');
+        int count = 0;
+
+        for(char ch : s.toCharArray()){
+            if(vowelset.contains(ch)){
+                count++;
+            }
+        }
+        if(count == 1){
+            return true;
+        }
+        if(count == 0 ){
+            return false;
+        }
+        
+        if(count % 2 != 0 || count % 2 == 0){
+            return true;
+        }
+        
+        return true;
+    }
+}
